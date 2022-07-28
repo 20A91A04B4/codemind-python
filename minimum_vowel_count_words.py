41c0d8ff04bd1b21ad4dbe343b3066a20cd29eb2
Divya sri Srikakulapu <84381735+20A91A04B4@users.noreply.github.com>
2022-07-28 20:56:02 +0530

    n=input().split() v='aeiouAEIOU' k=[] l=list(n) for i in l:     c=0     for j in i:         if j in v:             c+=1     k.append(c) m=min(k) p=[] for e in k:     if m==e:         p.append(e) print(len(p))

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,19 @@
+n=input().split()
+v='aeiouAEIOU'
+k=[]
+l=list(n)
+for i in l:
+    c=0
+    for j in i:
+        if j in v:
+            c+=1
+    k.append(c)
+m=min(k)
+p=[]
+for e in k:
+    if m==e:
+        p.append(e)
+print(len(p))
+
+    
+
